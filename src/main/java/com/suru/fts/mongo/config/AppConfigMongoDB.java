75e724506bfc60d03e3fc1976caf169691a4ba5d
Surendra Bajracharya <sbajracharya@sourceallies.com>
2017-10-05 21:57:54 -0500

    Updated the mongo config to include username and pwd and also modified the mongoDB config

--- src/main/java/com/suru/fts/mongo/config/AppConfigMongoDB.java
+++ src/main/java/com/suru/fts/mongo/config/AppConfigMongoDB.java
@@ -1,43 +1,57 @@
 package com.suru.fts.mongo.config;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientOptions;
+import com.mongodb.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.data.authentication.UserCredentials;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+
+import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 @Configuration
 @ComponentScan(basePackages = { "com.suru.fts.*" })
 @PropertySource("classpath:bootstrap.properties")
 public class AppConfigMongoDB {
 
-    @Value("${mongodb.url}")
-    private String mongodbUrl;
+    @Value("${mongodb.name}")
+    private String  dbName;
 
-    //hello
-    @Value("${mongodb.db}")
-    private String defaultDb;
+    @Value("${mongodb.host}")
+    private String  host;
 
-    @Bean
-    public MongoTemplate mongoTemplate() throws Exception {
+    @Value("${mongodb.port}")
+    private Integer port;
 
-        MongoClientOptions mongoOptions =
-                new MongoClientOptions.Builder().maxWaitTime(1000 * 60 * 5).build();
-        MongoClient mongo = new MongoClient(mongodbUrl, mongoOptions);
-        MongoDbFactory mongoDbFactory = new SimpleMongoDbFactory(mongo, defaultDb);
-        return new MongoTemplate(mongoDbFactory);
+    @Value("${mongodb.username}")
+    private String  userName;
 
+    @Value("${mongodb.password}")
+    private String  password;
+
+
+public @Bean MongoClient mongoClient() throws UnknownHostException {
+    return new MongoClient(new ServerAddress(host, port), new ArrayList<MongoCredential>() {
+
+        {
+            add(MongoCredential.createCredential(userName, dbName, password.toCharArray()));
+        }
+    });
+}
+
+    public @Bean MongoDbFactory mongoDbFactory() throws Exception {
+        return new SimpleMongoDbFactory(mongoClient(), dbName);
     }
 
-    //To resolve ${} in @Value
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
-        return new PropertySourcesPlaceholderConfigurer();
+    public @Bean MongoTemplate mongoTemplate() throws Exception {
+        MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
+        return mongoTemplate;
     }
+
 }
