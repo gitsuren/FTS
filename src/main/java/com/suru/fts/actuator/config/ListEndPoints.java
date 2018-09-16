40cc20291e64a7b52de0b2315001c9bd2cb5f97d
Surendra Bajracharya <bajracharya.surendra@gmail.com>
2018-09-16 07:41:28 -0500

    Upgraded the spring version to 5.x

--- src/main/java/com/suru/fts/actuator/config/ListEndPoints.java
+++ src/main/java/com/suru/fts/actuator/config/ListEndPoints.java
@@ -1,25 +1,27 @@
-package com.suru.fts.actuator.config;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.endpoint.AbstractEndpoint;
-import org.springframework.boot.actuate.endpoint.Endpoint;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
-
-@Component
-public class ListEndPoints extends AbstractEndpoint<List<Endpoint>> {
-    private List<Endpoint> endpoints;
-
-    @Autowired
-    public ListEndPoints(List<Endpoint> endpoints) {
-        super("listEndpoints");
-        this.endpoints = endpoints;
-    }
-
-
-    public @ResponseBody List<Endpoint> invoke() {
-        return this.endpoints;
-    }
-}
+//package com.suru.fts.actuator.config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.actuate.endpoint.AbstractEndpoint;
+//import org.springframework.boot.actuate.endpoint.AbstractExposableEndpoint;
+//import org.springframework.boot.actuate.endpoint.Endpoint;
+//import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//import java.util.List;
+//
+//@Component
+//public class ListEndPoints extends AbstractExposableEndpoint<List<Endpoint>> {
+//    private List<Endpoint> endpoints;
+//
+//    @Autowired
+//    public ListEndPoints(List<Endpoint> endpoints) {
+//        super("listEndpoints");
+//        this.endpoints = endpoints;
+//    }
+//
+//
+//    public @ResponseBody List<Endpoint> invoke() {
+//        return this.endpoints;
+//    }
+//}
--- src/main/java/com/suru/fts/controller/ActuatorController.java
+++ src/main/java/com/suru/fts/controller/ActuatorController.java
@@ -1,31 +1,31 @@
-package com.suru.fts.controller;
-
-import com.suru.fts.actuator.config.CustomEndpoint;
-import com.suru.fts.actuator.config.ListEndPoints;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.endpoint.Endpoint;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-@RestController
-public class ActuatorController {
-
-    @Autowired
-    ListEndPoints listEndPoints;
-
-    @Autowired
-    CustomEndpoint customEndpoint;
-
-    @GetMapping(value = "listEndPoints")
-    public List<Endpoint> listEndPoints() {
-        return listEndPoints.invoke();
-    }
-
-    @GetMapping(value = "customEndpoint")
-    public List<String> customEndpoint() {
-        return customEndpoint.invoke();
-    }
-
-}
+//package com.suru.fts.controller;
+//
+//import com.suru.fts.actuator.config.CustomEndpoint;
+//import com.suru.fts.actuator.config.ListEndPoints;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.actuate.endpoint.Endpoint;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.List;
+//
+//@RestController
+//public class ActuatorController {
+//
+//    @Autowired
+//    ListEndPoints listEndPoints;
+//
+//    @Autowired
+//    CustomEndpoint customEndpoint;
+//
+//    @GetMapping(value = "listEndPoints")
+//    public List<Endpoint> listEndPoints() {
+//        return listEndPoints.invoke();
+//    }
+//
+//    @GetMapping(value = "customEndpoint")
+//    public List<String> customEndpoint() {
+//        return customEndpoint.invoke();
+//    }
+//
+//}
